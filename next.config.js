f3e3a8879990cadafad5e44c4526d51ae23f4c34
RealSaake <youaretube719@gmail.com>
2025-08-22 01:21:42 +0700

    Fix build: add jsconfig.json, update next.config.js with webpack path resolution

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
